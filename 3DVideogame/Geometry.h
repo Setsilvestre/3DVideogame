21b20d88afc76e24cb529fe1527a602923347c6e
Xavi Figuerola <xavier_figuerola@hotmail.com>
2015-03-07 17:08:18 +0100

    17:08

---
+++ 3DVideogame/Geometry.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <glm/glm.hpp>			//OpenGL Mathematics 
+#include "Vertex.h"
+#include "GameElement.h"
+#include <vector>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace std;
+
+class Geometry
+{
+	vector <GameElement> gameElements;
+	Vertex *_data;
+	int numberOfPrimitives;
+public:
+	Geometry();
+	~Geometry();
+	void LoadScene();
+	void CreateCube(int position);
+	Vertex* ReturnData();
+	glm::vec3 returnScale(int i);
+	void Translate(int object, float x, float y, float z);
+	glm::vec3 returnTranslate(int i);
+	float returnAngle(int i);
+	glm::vec3 returnRotate(int i);
+	int returnPrimitives();
+	void Move(int i, int j);
+};
+
