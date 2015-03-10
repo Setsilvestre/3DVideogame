13c40e750686901fb4d08355d39d29979521a137
Xavi Figuerola <xavier_figuerola@hotmail.com>
2015-03-10 16:06:39 +0100

    Camera Updated1 Last Hope
    10/03/2015

--- 3DVideogame/3DVideogame/GameElement.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <glm/glm.hpp>			//OpenGL Mathematics 
-struct GameElement {
-	glm::fvec3 translate;
-	float angle;
-	glm::fvec3 rotate;
-	glm::fvec3 scale;
-	int idObject;
-};
